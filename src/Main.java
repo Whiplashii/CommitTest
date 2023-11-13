22293e9a5c880d4924daf10a2c37900222734cc2
Sup-Old <s.naumkin10@gmail.com>
2023-11-13 12:56:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("qwe");
+        System.out.println("zxc");
+        System.out.println("Test commit");
+        System.out.println("Ski");
+    }
+}
